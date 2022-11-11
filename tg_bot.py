ff25b55069eccedc29ffa57d066b886b64efcb59
Victor Abdukarimov <viredmi@gmail.com>
2022-11-11 18:23:25 +0300

    Added logging

--- tg_bot.py
+++ tg_bot.py
@@ -1,18 +1,24 @@
 import os
 import logging
 
-from google.cloud import storage, dialogflow
+from google.cloud import dialogflow
 from dotenv import load_dotenv
 
+import telegram
 from telegram import Update
 from telegram.ext import CallbackContext, MessageHandler, Filters, Updater
 
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-)
-logger = logging.getLogger("supportbot")
+class TgbotLogger(logging.Handler):
+    
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def detect_intent_texts(text, language_code):
@@ -35,18 +41,32 @@ def detect_intent_texts(text, language_code):
 def send_reply(update: Update, context: CallbackContext):
     response_text = detect_intent_texts(update.message.text, "ru-RU")
     if response_text:
-        context.bot.send_message(chat_id=update.effective_chat.id, text=response_text)
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=response_text
+        )
     
 
 def main() -> None:
     load_dotenv()
-    updater = Updater(token=os.getenv("TGBOT_TOKEN"), use_context=True)
-    dispatcher = updater.dispatcher
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), send_reply)
-    dispatcher.add_handler(echo_handler)
-    updater.start_polling()
-    updater.idle()
-
+    logger = logging.getLogger("supportbot")
+    logger_bot = telegram.Bot(token=str(os.getenv("TG_LOGGER_TOKEN")))
+    logger.setLevel(logging.WARNING)
+    bot_logger = TgbotLogger(logger_bot, os.getenv("GG_DF_SESSION_ID"))
+    logger.addHandler(bot_logger)
+    try:
+        updater = Updater(token=os.getenv("TGBOT_TOKEN"), use_context=True)
+        dispatcher = updater.dispatcher
+        echo_handler = MessageHandler(
+            Filters.text & (~Filters.command),
+            send_reply
+        )
+        dispatcher.add_handler(echo_handler)
+        updater.start_polling()
+        logger.warning("Telegram bot поддержки запущен!")
+        updater.idle()
+    except Exception as e:
+        logger.error(f"Telegram bot поддержки упал с ошибкой:\n{e}")
 
 if __name__ == "__main__":
     main()
--- vk_bot.py
+++ vk_bot.py
@@ -1,10 +1,24 @@
 import os
 import random
+import logging
 
+import telegram
 from google.cloud import dialogflow
 from dotenv import load_dotenv
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+class TgbotLogger(logging.Handler):
+    
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def detect_intent_texts(text, language_code):
@@ -36,11 +50,19 @@ def send_reply(event, vk_api):
 
 if __name__ == "__main__":
     load_dotenv()
+    logger = logging.getLogger("supportbot")
+    logger_bot = telegram.Bot(token=str(os.getenv("TG_LOGGER_TOKEN")))
+    logger.setLevel(logging.WARNING)
+    bot_logger = TgbotLogger(logger_bot, os.getenv("GG_DF_SESSION_ID"))
+    logger.addHandler(bot_logger)
     vk_session = vk_api.VkApi(token=os.getenv("VK_API_KEY"))
     vk_api = vk_session.get_api()
-    longpoll = VkLongPoll(vk_session)
-
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-           send_reply(event, vk_api)
+    try:
+        longpoll = VkLongPoll(vk_session)
+        logger.warning("VK bot поддержки запущен!")
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+               send_reply(event, vk_api)
+    except Exception as e:
+        logger.error(f"VK bot упал с ошибкой:\n{e}")
 
